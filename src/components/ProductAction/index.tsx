523560f287186744dbafa945ebe4e9995450891c
Diego Vespa <diego-vespa2020@outlook.com.br>
2020-09-02 21:41:43 -0300

    finalizando projeto

--- src/components/ProductAction/index.tsx
+++ src/components/ProductAction/index.tsx
@@ -53,7 +53,7 @@ const ProductAction: React.FC = () => {
        </div>
      </MethodCard>
      <Actions>
-       <Button >Comprar agora</Button>
+       <Button solid >Comprar agora</Button>
        <Button >Adicionar ao carrinho</Button>
      </Actions>
 
